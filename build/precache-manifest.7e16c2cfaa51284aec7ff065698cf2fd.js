929c453b7bcc25269ce64747a3ca087c7b6f007f
Maxmud <88928106+atorud19851908@users.noreply.github.com>
2022-02-22 22:15:48 +0500

    Add files via upload

---
+++ build/precache-manifest.7e16c2cfaa51284aec7ff065698cf2fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dfc718869da49dfd71714f89651f46d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f29649dca26d2c5a68ea",
+    "url": "/static/css/main.6c780ff7.chunk.css"
+  },
+  {
+    "revision": "d289785d45eb1d0e361c",
+    "url": "/static/js/2.11a6c8ab.chunk.js"
+  },
+  {
+    "revision": "8090c68024a1d593772d484b9af6b7f0",
+    "url": "/static/js/2.11a6c8ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f29649dca26d2c5a68ea",
+    "url": "/static/js/main.5b3620d5.chunk.js"
+  },
+  {
+    "revision": "28c7534599cac65bece8",
+    "url": "/static/js/runtime-main.84cecf6d.js"
+  }
+]);
